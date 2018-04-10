43578b86edb831fb7297186f82f3f5b566e4d446
landhuang <jybbh@163.com>
2018-04-10 23:25:08 +0800

    备份一下

--- src/main/java/com/land/jeten/customer/iface/ICustomerRestService.java
+++ src/main/java/com/land/jeten/customer/iface/ICustomerRestService.java
@@ -1,30 +1,22 @@
 package com.land.jeten.customer.iface;
 
 import com.land.jeten.mybatis.model.CustomerModel;
-import com.land.jeten.vo.Customer;
-import com.land.jeten.vo.LoginUser;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 import java.util.Map;
 
 @RequestMapping(value = "/customer")
 public interface ICustomerRestService {
-  @RequestMapping(value = "/", method = RequestMethod.GET)
-  List<CustomerModel> getCustomerList(Map<String, Object> map);
+  @RequestMapping(value = "/select", method = RequestMethod.POST)
+  Map<String,Object> selectCustomer(@ModelAttribute Map<String,Object> mapPara);
 
-  @RequestMapping(value = "/", method = RequestMethod.POST)
-  Map<String,Object> postCustomer(@ModelAttribute CustomerModel customer);
+  @RequestMapping(value = "/insert", method = RequestMethod.POST)
+  Map<String, Object> insertCustomer(@RequestBody CustomerModel customer);
+
+  @RequestMapping(value = "/update", method = RequestMethod.POST)
+  Map<String, Object> updateCustomer(@RequestBody CustomerModel customer);
 
   @RequestMapping(value = "/{customerID}", method = RequestMethod.GET)
   CustomerModel getCustomer(@PathVariable String customerID);
 
-  @RequestMapping(value="/{customerID}", method=RequestMethod.PUT)
-  Map<String,Object> putCustomer(@PathVariable String customerID, @ModelAttribute CustomerModel customer);
-
-  @RequestMapping(value="/{customerID}", method=RequestMethod.DELETE)
-  Map<String,Object> deleteCustomer(@PathVariable String customerID);
 }
