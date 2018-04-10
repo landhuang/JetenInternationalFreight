b662a98a93ad0a35c44c72c08e8b1a88e7b9e45a
landhuang <jybbh@163.com>
2018-04-10 23:24:43 +0800

    备份一下

--- src/main/java/com/land/jeten/customer/impl/CustomerRestServiceImpl.java
+++ src/main/java/com/land/jeten/customer/impl/CustomerRestServiceImpl.java
@@ -13,30 +13,13 @@ import java.util.*;
 @RestController
 public class CustomerRestServiceImpl implements ICustomerRestService {
 
+    private static String strTableName = "customerinfo";
+
     @Autowired
     private CustomerMapper customerMapper;
 
-//    @Override
-//    public List<CustomerModel> getCustomerList() {
-//
-////        Object obj  = map.get("pageNo");
-////        String obj  = map.get("pageNo");
-//        int pageNo= 1;
-////        if(obj != null){
-////            pageNo =  Integer.parseInt(obj.toString());
-////        }
-//        if (pageNo > 0) {
-//            PageHelper.startPage(pageNo,3); // 设置分页，参数1=页数，参数2=每页显示条数
-//
-//        }
-//
-//        List<CustomerModel> customerList = customerMapper.getAll();
-//        return customerList;
-//    }
-
     @Override
     public Map<String, Object> selectCustomer(@RequestBody Map<String,Object> mapPara) {
-
         Object objPageSize  = mapPara.get("pageSize");
         Object objPageNo  = mapPara.get("pageNo");
         int pageNo= 1;
@@ -49,54 +32,33 @@ public class CustomerRestServiceImpl implements ICustomerRestService {
         }
         if (pageNo > 0) {
             PageHelper.startPage(pageNo,pageSize); // 设置分页，参数1=页数，参数2=每页显示条数
-
         }
-
         List<CustomerModel> customerList = customerMapper.getAll(mapPara);
-
-
-
         Map<String, Object> map = new Hashtable<>();
         map.put("message", "成功");
         map.put("returnObject", customerList);
         return map;
     }
-
     @Override
-    public Map<String, Object> postCustomer(CustomerModel customer) {
-        customer.setCustomerID("customerinfo"+UUID.randomUUID().toString());
+    public Map<String, Object> insertCustomer(@RequestBody CustomerModel customer) {
+        customer.setCustomerID(strTableName+UUID.randomUUID().toString());
         customerMapper.insert(customer);
         Map<String, Object> map = new Hashtable<>();
         map.put("message", "成功");
+        map.put("returnObject", customer);
         return map;
     }
-
+    @Override
+    public Map<String, Object> updateCustomer(@RequestBody CustomerModel customer) {
+        customerMapper.update(customer);
+        Map<String, Object> map = new Hashtable<>();
+        map.put("message", "成功");
+        map.put("returnObject", customer);
+        return map;
+    }
     @Override
     public CustomerModel getCustomer(@PathVariable String customerID) {
         System.out.println("customerID:" + customerID + "]");
         return customerMapper.getOne(customerID);
     }
-
-    @Override
-    public Map<String, Object> putCustomer(@PathVariable String customerID, CustomerModel customer) {
-        customerMapper.update(customer);
-        Map<String, Object> map = new Hashtable<>();
-        map.put("message", "成功");
-        return map;
-    }
-
-    @Override
-    public Map<String, Object> deleteCustomer(@PathVariable String customerID) {
-        Map<String, Object> map = new Hashtable<>();
-        map.put("message", "成功");
-        return map;
-    }
-
-
-//    public List<CustomerModel> selectStudentByPage(@PathVariable int pageNo) {
-//        if (pageNo > 0) {
-//            PageHelper.startPage(pageNo,3); // 设置分页，参数1=页数，参数2=每页显示条数
-//        }
-//        return customerMapper.getAll();
-//    }
 }
